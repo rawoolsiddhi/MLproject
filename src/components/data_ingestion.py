19fe1eb1cdde11c204d7149453aeca0a6ed83489
rawoolsiddhi <rawoolsiddhi814@gmail.com>
2024-06-27 01:45:58 +0530

    data tranformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,8 @@ from src.logger import logging
 
 
 from dataclasses import dataclass
-
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 # from src.components.data_transformation import DataTransformation
 # from src.components.data_transformation import DataTransformationConfig
@@ -55,3 +55,6 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
